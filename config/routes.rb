bae64418ac03bdf322099c58617cdf7f27c48a9c
John Royer <john.h.royer@gmail.com>
2012-03-02 00:08:23 -0500

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 SampleApp::Application.routes.draw do
  
+ get "users/new"
 
+root :to => 'pages#home'
+  match '/signup',  :to => 'users#new'
 
-    match '/contact', :to => 'pages#contact'
+  match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-root :to => 'pages#home'
 
-root_path => '/'
-root_url  => 'http://localhost:3000/'
+  
+
+
 
 
   # The priority is based upon order of creation:
