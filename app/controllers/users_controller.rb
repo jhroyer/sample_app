bb0688ebe6612fccf4f042eec2b0b4c6ae2d4a2c
John Royer <john.h.royer@gmail.com>
2012-04-01 12:56:27 -0400

    Ch 8 Exercises completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,15 +12,16 @@ def new
 
   end
 
-def create
-    @user = User.new(params[:user])
-    if @user.save
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      @title = "Sign up"
-      render 'new'
-    end
-  end
+def create     
+@user = User.new(params[:user])    if @user.save      
+sign_in @user      
+flash[:success] = "Welcome to the Sample App!"      
+redirect_to @user    
+else      
+@title = "Sign up"    
+@user.password = ""
+@user.password_confirmation = ""      
+render :new    
+end  
 end
-
+end
